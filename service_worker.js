2e45cf163b1c87726a5af252091257e8733ca07a
Nakamura <>
2018-12-13 16:41:38 +0900

    no message

--- service_worker.js
+++ service_worker.js
@@ -1,23 +1,27 @@
-self.addEventListener('fetch', function (event) {
- event.respondWith(
-  new Response('サービスワーカーが動いています！')
- );
-});
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches-v1-001';
+var urlsToCache = [
+ '/poster-keisuke.github.io/',
+];
 
-self.addEventListener('push', function (event) {
- console.log('Received a push message', event);
- var title = "プッシュ通知です！";
- var body = "プッシュ通知はこのようにして送られるのです";
-
+// インストール処理
+self.addEventListener('install', function(event) {
  event.waitUntil(
-  self.registration.showNotification(title, {
-   body: body,
-   icon: 'http://free-images.gatag.net/images/201108090000.jpg',
-   tag: 'push-notification-tag'
+  caches
+  .open(CACHE_NAME)
+  .then(function(cache) {
+   return cache.addAll(urlsToCache);
   })
  );
 });
-self.addEventListener('notificationclick', function (event) {
- event.notification.close();
- clients.openWindow("/");
-}, false);
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+ event.respondWith(
+  caches
+  .match(event.request)
+  .then(function(response) {
+   return response ? response : fetch(event.request);
+  })
+ );
+});
